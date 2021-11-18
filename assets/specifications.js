7e35a6783b77fc1fe483e2b8fb0576f1dfac2bc1
Fredrik Karlsson <flommy@outlook.com>
2021-11-18 14:04:06 +0100

    Updated renderer to include null values

--- assets/specifications.js
+++ assets/specifications.js
@@ -1,3 +1,5 @@
 App.Utils.renderer.specifications = function (v) {
-    return typeof v === 'object' && '<span class="uk-badge" title="' + v.templateId + '">' + Object.keys(v.values).length + '</span>' || "";
+    return v != null && typeof v === 'object'
+        ? '<span class="uk-badge" title="' + v.templateId + '">' + Object.keys(v.values).length + '</span>'
+        : '<span class="uk-icon-eye-slash uk-text-muted"></span>';
 };
